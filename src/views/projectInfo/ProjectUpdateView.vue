e44788d3cf5f187b9f1fe8f9306b1740c65d5015
Chohanbin0511 <hanbin159@naver.com>
2022-09-15 17:18:45 +0900

    업데이트 현황

--- src/views/projectInfo/ProjectUpdateView.vue
+++ src/views/projectInfo/ProjectUpdateView.vue
@@ -3,6 +3,19 @@
 		<template #mainPanel>
 			<v-container>
 				<h1>Update....</h1>
+				<v-timeline class="mt-15">
+					<v-timeline-item
+						v-for="item in updateInfoList"
+						:key="item"
+						:dot-color="item.color"
+						size="x-small"
+					>
+						{{ item.time }}
+						<div class="font-weight-normal">
+							<strong>{{ item.text }}</strong>
+						</div>
+					</v-timeline-item>
+				</v-timeline>
 			</v-container>
 			<!-- 검색 창-->
 		</template>
@@ -11,6 +24,20 @@
 
 <script setup>
 import TheViewLayout from '@/layouts/TheViewLayout.vue';
+import { ref } from 'vue';
+
+const updateInfoList = ref([
+	{
+		color: 'green',
+		text: 'layout 조정',
+		time: '2022/09/15',
+	},
+	{
+		color: 'yellow',
+		text: 'git Test',
+		time: '2022/09/14',
+	},
+]);
 </script>
 
 <style lang="scss" scoped></style>
